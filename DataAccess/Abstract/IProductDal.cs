f15878278ff9234c3649fe900b075ac6ca3aff6e
yunus_yngl <57035421+yunusyngl@users.noreply.github.com>
2021-01-31 00:44:36 +0300

    Add project files.

---
+++ DataAccess/Abstract/IProductDal.cs
@@ -0,0 +1,19 @@
+﻿using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Abstract
+{
+    public interface IProductDal //interface public değil ama operasyonları yani add, update, delete bunlar publix
+    {
+        List<Product> GetAll();
+        void Add(Product product);
+        void Update(Product product);
+        void Delete(Product product);
+
+        List<Product> GetAllByCategory(int categoryId); // bütün ürünleri categorilere göre filtrele
+
+
+    }
+}
---
+++ Entities/Concrete/Product.cs
@@ -0,0 +1,18 @@
+﻿using Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Concrete
+{
+    public class Product:IEntity// bu class a diğer katmanlarda görsün diye class ın başına public yazdık // internal yazarsak class ın başına  o vakit sadece entites katmanı ulaşır
+    {
+        public int ProductId { get; set; }
+        public int CategoryId { get; set; }
+        public string ProductName { get; set; }
+        public short UnitInStock { get; set; }//stok adedi
+        public decimal UnitPrice { get; set; }
+
+
+    }
+}
